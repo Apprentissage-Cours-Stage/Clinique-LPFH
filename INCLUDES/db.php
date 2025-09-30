8cbac648152c95a2f39f9c81ea355747719731d8
Malthael <ma.1thea1.wright@gmail.com>
2025-09-30 09:45:20 +0200

    Ajout de la connexion de base avec la BDD (non sécurisé)

---
+++ INCLUDES/db.php
@@ -0,0 +1,15 @@
+<?php
+//Changer le compte selon le compte utiliser (SECRETARY, SECRET@RYLPFS2025)/(ADMINISTRATEUR)
+$host = "192.168.20.15";
+$user = "root";
+$password = "sio2025";
+$dbname = "CliniqueLPFS";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+//Verification de la connexion
+if ($conn->connect_error) {
+    die("Connexion échouée: ".$conn->connect_error);
+}
+//Encodage UTF-8
+$conn->set_charset("utf8");
+?>
