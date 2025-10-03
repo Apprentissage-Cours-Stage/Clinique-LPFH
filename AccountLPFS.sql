7f9faca96ea10dd3ab4627415f5dd69a64c72dc4
Malthael <ma.1thea1.wright@gmail.com>
2025-10-03 14:57:40 +0200

    Ajout des 2 comptes nominatifs à la BDD

---
+++ AccountLPFS.sql
@@ -0,0 +1,24 @@
+-- Compte d'authentification
+CREATE USER 'AuthentificationLPFS2025'@'%' IDENTIFIED BY 'AuthLPFS2025';
+
+GRANT SELECT ON `CliniqueLPFS`.`Personnel` TO `AuthentificationLPFS2025`@`%`;
+GRANT SELECT ON `CliniqueLPFS`.`Role` TO `AuthentificationLPFS2025`@`%`;
+GRANT SELECT ON `CliniqueLPFS`.`Utilisateurs` TO `AuthentificationLPFS2025`@`%`;
+
+-- Compte ClaireDPT
+CREATE USER `ClaireDPT_secretariat`@`%` IDENTIFIED BY PASSWORD 'Cl@ireDPT1993';
+GRANT SELECT ON `CliniqueLPFS`.`TypeHospitalisation` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT ON `CliniqueLPFS`.`Patient` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `CliniqueLPFS`.`Personnel` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT ON `CliniqueLPFS`.`CouvertureSocial` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `CliniqueLPFS`.`Civilité` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `CliniqueLPFS`.`TypeChambre` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT ON `CliniqueLPFS`.`PreAdmission` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `CliniqueLPFS`.`Role` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `CliniqueLPFS`.`Chambre` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT ON `CliniqueLPFS`.`Hospitalisation` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT ON `CliniqueLPFS`.`Personne_prevenir` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT ON `CliniqueLPFS`.`SousTutellede` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT ON `CliniqueLPFS`.`PiecesJoints` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `CliniqueLPFS`.`Service` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT ON `CliniqueLPFS`.`Responsable` TO `ClaireDPT_secretariat`@`%`;
