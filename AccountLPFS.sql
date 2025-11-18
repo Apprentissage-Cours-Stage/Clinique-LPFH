8c3f14993494766efcaceec69ff37b18b7e44ab4
Malthael <ma.1thea1.wright@gmail.com>
2025-11-18 08:31:21 +0100

    Mise à jour des privilèges du compte secrétaire

--- AccountLPFS.sql
+++ AccountLPFS.sql
@@ -7,20 +7,20 @@ GRANT SELECT ON `CliniqueLPFS`.`Utilisateurs` TO `AuthentificationLPFS2025`@`%`;
 -- Compte ClaireDPT
 CREATE USER `ClaireDPT_secretariat`@`%` IDENTIFIED BY 'Cl@ireDPT1993';
 GRANT SELECT ON `CliniqueLPFS`.`TypeHospitalisation` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT ON `CliniqueLPFS`.`Patient` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`Patient` TO `ClaireDPT_secretariat`@`%`;
 GRANT SELECT ON `CliniqueLPFS`.`Personnel` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT ON `CliniqueLPFS`.`CouvertureSocial` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`CouvertureSocial` TO `ClaireDPT_secretariat`@`%`;
 GRANT SELECT ON `CliniqueLPFS`.`Civilité` TO `ClaireDPT_secretariat`@`%`;
 GRANT SELECT ON `CliniqueLPFS`.`TypeChambre` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT ON `CliniqueLPFS`.`PreAdmission` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`PreAdmission` TO `ClaireDPT_secretariat`@`%`;
 GRANT SELECT ON `CliniqueLPFS`.`Role` TO `ClaireDPT_secretariat`@`%`;
 GRANT SELECT ON `CliniqueLPFS`.`Chambre` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT ON `CliniqueLPFS`.`Hospitalisation` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT ON `CliniqueLPFS`.`Personne_prevenir` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT ON `CliniqueLPFS`.`SousTutellede` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT ON `CliniqueLPFS`.`PiecesJoints` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`Hospitalisation` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`Personne_prevenir` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`SousTutellede` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`PiecesJoints` TO `ClaireDPT_secretariat`@`%`;
 GRANT SELECT ON `CliniqueLPFS`.`Service` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT ON `CliniqueLPFS`.`Responsable` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`Responsable` TO `ClaireDPT_secretariat`@`%`;
 
 -- Compte ADMINLPFS
 CREATE USER `ADMINLPFS`@`%` IDENTIFIED BY 'ADMINLPFS2025';
