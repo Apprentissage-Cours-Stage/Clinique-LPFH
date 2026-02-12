e477267a05c108fc14608210800b2dd671c5358d
Malthael <ma.1thea1.wright@gmail.com>
2026-02-12 09:16:40 +0100

    Ajout des liens vers le listing des admissions

---
+++ SECRETARY/list-admission.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des Pré-admission - Secrétaire</title>
+    <link rel="stylesheet" href="../CSS/add-admission.css">
+</head>
+
+<body>
+    <div class="dashboard-container">
+        <div class="sidebar">
+            <img src="../INCLUDES/IMAGES/LPFSLogo.png" alt="Logo Clinique" class="logo">
+            <h2>Panel Secrétaire</h2>
+            <ul class="menu">
+                <li><a href="dashboard-secretary.php" style="color:#fff; text-decoration:none;">Accueil</a></li>
+                <li><a href="add-admission.php" style="color:#fff; text-decoration:none;">Enregistrer une Pré-admission</a></li>
+                <li><a href="list-admission.php" style="color:#fff; text-decoration:none;">Liste des Pré-admissions</a></li>
+                <li><a href="../logout.php" style="color:#fff; text-decoration:none;">Déconnexion</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
