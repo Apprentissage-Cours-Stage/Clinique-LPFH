32800d9e389c7e0cb4f6d9e62e405a5d8766d780
Malthael <ma.1thea1.wright@gmail.com>
2026-02-12 10:38:41 +0100

    Ajout de la page d'ajout de service

---
+++ ADMIN/add-service.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajout d'un Service - Administrateur</title>
+    <link rel="stylesheet" href="../CSS/add-admission.css">
+</head>
+<body>
+    <div class="dashboard-container">
+        <div class="sidebar">
+            <img src="../INCLUDES/IMAGES/LPFSLogo.png" alt="Logo Clinique" class="logo">
+            <h2>Panel Administrateur</h2>
+            <ul class="menu">
+                <li><a href="dashboard-admin.php" style="color:#fff; text-decoration:none;">Accueil</a></li>
+                <li><a href="add-admission.php" style="color:#fff; text-decoration:none;">Enregistrer une Pré-admission</a></li>
+                <li><a href="list-admission.php" style="color:#fff; text-decoration:none;">Liste des Pré-admissions</a></li>
+                <li>Enregistrer un nouveau personnel/utilisateur</li>
+                <li>Liste du personnels/utilisateurs</li>
+                <li><a href="add-service.php" style="color:#fff; text-decoration:none;">Enregistrer un nouveau service</a></li>
+                <li>Liste des services</li>
+                <li><a href="../logout.php" style="color:#fff; text-decoration:none;">Déconnexion</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
