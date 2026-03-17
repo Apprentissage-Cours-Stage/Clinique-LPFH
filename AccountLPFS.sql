7bf4eb55c0657f7dc6390f54fb29c1d7afef2291
Malthael <ma.1thea1.wright@gmail.com>
2026-03-17 08:43:32 +0100

    Mise à jour des nom de base pour le LAMP externe

--- AccountLPFS.sql
+++ AccountLPFS.sql
@@ -1,27 +1,28 @@
 -- Compte d'authentification
 CREATE USER 'AuthentificationLPFS2025'@'%' IDENTIFIED BY 'AuthLPFS2025';
-GRANT SELECT ON `CliniqueLPFS`.`Personnel` TO `AuthentificationLPFS2025`@`%`;
-GRANT SELECT ON `CliniqueLPFS`.`Role` TO `AuthentificationLPFS2025`@`%`;
-GRANT SELECT ON `CliniqueLPFS`.`Utilisateurs` TO `AuthentificationLPFS2025`@`%`;
+GRANT SELECT ON `cliniquelpfs`.`personnel` TO `AuthentificationLPFS2025`@`%`;
+GRANT SELECT ON `cliniquelpfs`.`role` TO `AuthentificationLPFS2025`@`%`;
+GRANT SELECT ON `cliniquelpfs`.`utilisateurs` TO `AuthentificationLPFS2025`@`%`;
 
 -- Compte ClaireDPT
 CREATE USER `ClaireDPT_secretariat`@`%` IDENTIFIED BY 'Cl@ireDPT1993';
-GRANT SELECT ON `CliniqueLPFS`.`TypeHospitalisation` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`Patient` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT ON `CliniqueLPFS`.`Personnel` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`CouvertureSocial` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT ON `CliniqueLPFS`.`Civilité` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT ON `CliniqueLPFS`.`TypeChambre` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`PreAdmission` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT ON `CliniqueLPFS`.`Role` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT ON `CliniqueLPFS`.`Chambre` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`Hospitalisation` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`Personne_prevenir` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`SousTutellede` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`PiecesJoints` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT ON `CliniqueLPFS`.`Service` TO `ClaireDPT_secretariat`@`%`;
-GRANT SELECT, INSERT, UPDATE, DELETE ON `CliniqueLPFS`.`Responsable` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `cliniquelpfs`.`typehospitalisation` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `cliniquelpfs`.`personnel` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `cliniquelpfs`.`couverturesocial` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `cliniquelpfs`.`civilité` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `cliniquelpfs`.`chambre` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `cliniquelpfs`.`typechambre` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `cliniquelpfs`.`personne_prevenir` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `cliniquelpfs`.`role` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `cliniquelpfs`.`patient` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `cliniquelpfs`.`hospitalisation` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `cliniquelpfs`.`preadmission` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `cliniquelpfs`.`soustutellede` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `cliniquelpfs`.`piecesjoints` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON `cliniquelpfs`.`responsable` TO `ClaireDPT_secretariat`@`%`;
+GRANT SELECT ON `cliniquelpfs`.`service` TO `ClaireDPT_secretariat`@`%`;
 
 -- Compte ADMINLPFS
 CREATE USER `ADMINLPFS`@`%` IDENTIFIED BY 'ADMINLPFS2025';
-GRANT ALL PRIVILEGES ON `CliniqueLPFS`.* TO `ADMINLPFS`@`%`;
+GRANT CREATE USER ON *.* TO `ADMINLPFS`@`%` IDENTIFIED BY PASSWORD 'ADMINLPFS2025';
+GRANT ALL PRIVILEGES ON `cliniquelpfs`.* TO `ADMINLPFS`@`%` WITH GRANT OPTION;
