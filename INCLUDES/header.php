efda9ff582a4084e2e2b44369b0e42240e8dc4aa
Malthael <ma.1thea1.wright@gmail.com>
2026-03-24 14:25:57 +0100

    Mise à jour pour fonctionner avec les nouveaux dashboard

--- INCLUDES/header.php
+++ INCLUDES/header.php
@@ -5,16 +5,20 @@ if (!isset($basePath)) {
 
 if (!isset($context)) {
     exit;
-} 
+}
 ?>
 <div class="sidebar">
     <img src="<?= $basePath ?>/INCLUDES/IMAGES/LPFSLogo.png" alt="Logo Clinique" class="logo">
     <h2>Panel <?= htmlspecialchars($shownContext) ?></h2>
     <ul class="menu">
-        <li><a href="dashboard-<?= strtolower($context) ?>.php">Accueil</a></li>
-        <li><a href="add-admission.php">Enregistrer une Pré-admission</a></li>
-        <li><a href="list-admission.php">Liste des Pré-admissions</a></li>
-        <?php if ($context === "ADMIN"): ?>
+        <?php if ($context === "SECRETARY"): ?>
+            <li><a href="dashboard-<?= strtolower($context) ?>.php">Accueil</a></li>
+            <li><a href="add-admission.php">Enregistrer une Pré-admission</a></li>
+            <li><a href="list-admission.php">Liste des Pré-admissions</a></li>
+        <?php elseif ($context === "ADMIN"): ?>
+            <li><a href="dashboard-<?= strtolower($context) ?>.php">Accueil</a></li>
+            <li><a href="add-admission.php">Enregistrer une Pré-admission</a></li>
+            <li><a href="list-admission.php">Liste des Pré-admissions</a></li>
             <li><a href="add-user.php">Enregistrer un nouveau personnel/utilisateur</a></li>
             <li><a href="list-user.php">Liste du personnel/utilisateurs</a></li>
             <li><a href="add-service.php">Enregistrer un nouveau service</a></li>
